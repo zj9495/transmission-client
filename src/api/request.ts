474ac58282ce1ce415ec25be3d72ad5aee8815a3
zj9495 <zj9495@gmail.com>
2020-12-24 11:17:10 +0800

    fix: update env

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const request = axios.create({
-  baseURL: `${process.env.REACT_APP_}/transmission/rpc`,
+  baseURL: `${process.env.REACT_APP_TR_RPC_HOST}/transmission/rpc`,
   withCredentials: true
 });
 
