e9a0cc6b6d2d963972680d7baccdec7c788a215a
zj9495 <zj9495@gmail.com>
2021-01-02 22:16:42 +0800

    refactor: refactor ActionBar

--- src/components/ActionBar/index.tsx
+++ src/components/ActionBar/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
 import { Box, IconButton } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 import AddIcon from '@material-ui/icons/Add';
@@ -8,16 +7,7 @@ import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import PauseIcon from '@material-ui/icons/Pause';
 import StopIcon from '@material-ui/icons/Stop';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    margin: {
-      margin: theme.spacing(0),
-    },
-  }),
-);
-
 const ActionBar = () => {
-  const classes = useStyles()
 
   return (
     <Box>
