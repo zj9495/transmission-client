b888d881af71445a8c59c9d03368031da19ef29d
zj9495 <zj9495@gmail.com>
2021-01-13 22:58:37 +0800

    feat(utils): add time stamp formatter

--- src/utils/formatter.test.ts
+++ src/utils/formatter.test.ts
@@ -1,10 +1,15 @@
-import { formatBytes, formatSize, formatSpeed } from "./units";
-import store from '../store'
+import {
+  formatBytes,
+  formatSize,
+  formatSpeed,
+  formatUnixTimeStamp,
+} from "./formatter";
+import store from "../store";
 
 describe.each`
   number                                   | bytes   | units                       | digits       | expected
   ${1}                                     | ${1000} | ${["kB", "MB", "GB", "TB"]} | ${undefined} | ${"0kB"}
-  ${1210657698}                                     | ${1000} | ${["kB", "MB", "GB", "TB"]} | ${undefined} | ${"1.21GB"}
+  ${1210657698}                            | ${1000} | ${["kB", "MB", "GB", "TB"]} | ${undefined} | ${"1.21GB"}
   ${1000}                                  | ${1000} | ${["kB", "MB", "GB", "TB"]} | ${undefined} | ${"1kB"}
   ${3096}                                  | ${1000} | ${["kB", "MB", "GB", "TB"]} | ${undefined} | ${"3.1kB"}
   ${1000000}                               | ${1000} | ${["kB", "MB", "GB", "TB"]} | ${undefined} | ${"1MB"}
@@ -32,62 +37,98 @@ describe.each`
 );
 describe.each`
   number
-  ${Math.floor(Math.random()*1001)}
-  ${Math.floor(Math.random()*1001)}
-  ${Math.floor(Math.random()*1000001)}
-  ${Math.floor(Math.random()*1000001)}
-  ${Math.floor(Math.random()*1000000001)}
-  ${Math.floor(Math.random()*1000000001)}
-  ${Math.floor(Math.random()*1000000000001)}
-  ${Math.floor(Math.random()*1000000000001)}
-  ${Math.floor(Math.random()*1000000000000001)}
-  ${Math.floor(Math.random()*1000000000000001)}
-  ${Math.floor(Math.random()*10000000000000001)}
-  ${Math.floor(Math.random()*10000000000000001)}
-`(
-  "random test formatBytes",
-  ({ number }) => {
-    const units = ["kB", "MB", "GB", "TB"]
+  ${Math.floor(Math.random() * 1001)}
+  ${Math.floor(Math.random() * 1001)}
+  ${Math.floor(Math.random() * 1000001)}
+  ${Math.floor(Math.random() * 1000001)}
+  ${Math.floor(Math.random() * 1000000001)}
+  ${Math.floor(Math.random() * 1000000001)}
+  ${Math.floor(Math.random() * 1000000000001)}
+  ${Math.floor(Math.random() * 1000000000001)}
+  ${Math.floor(Math.random() * 1000000000000001)}
+  ${Math.floor(Math.random() * 1000000000000001)}
+  ${Math.floor(Math.random() * 10000000000000001)}
+  ${Math.floor(Math.random() * 10000000000000001)}
+`("random test formatBytes", ({ number }) => {
+  const units = ["kB", "MB", "GB", "TB"];
 
-    let expected: string
-    if (number < 1000000) {
-      expected = Number((number / 1000).toFixed(2)) + 'kB'
-    } else if (number < 1000000000) {
-      expected = Number((number / 1000000).toFixed(2)) + 'MB'
-    } else if (number < 1000000000000) {
-      expected = Number((number / 1000000000).toFixed(2)) + 'GB'
-    } else {
-      expected = Number((number / 1000000000000).toFixed(2)) + 'TB'
-    }
-    test(`${number} returns ${expected}`, () => {
-      expect(formatBytes(number, 1000, units)).toBe(expected);
-    });
+  let expected: string;
+  if (number < 1000000) {
+    expected = Number((number / 1000).toFixed(2)) + "kB";
+  } else if (number < 1000000000) {
+    expected = Number((number / 1000000).toFixed(2)) + "MB";
+  } else if (number < 1000000000000) {
+    expected = Number((number / 1000000000).toFixed(2)) + "GB";
+  } else {
+    expected = Number((number / 1000000000000).toFixed(2)) + "TB";
   }
-);
+  test(`${number} returns ${expected}`, () => {
+    expect(formatBytes(number, 1000, units)).toBe(expected);
+  });
+});
 
-test('formatSize', () => {
+test("formatSize", () => {
   store.getState = jest.fn().mockReturnValue({
     session: {
       units: {
         sizeBytes: 1000,
         sizeUnits: ["kB", "MB", "GB", "TB"],
-      }
+      },
     },
-  })
+  });
 
-  expect(formatSize(1000)).toBe('1kB')
-})
+  expect(formatSize(1000)).toBe("1kB");
+});
 
+test("formatSize format zero", () => {
+  store.getState = jest.fn().mockReturnValue({
+    session: {
+      units: {
+        sizeBytes: 1000,
+        sizeUnits: ["kB", "MB", "GB", "TB"],
+      },
+    },
+  });
 
-test('formatSpeed', () => {
+  expect(formatSize(0, true)).toBe("0kB");
+  expect(formatSize(0)).toBe(" ");
+});
+
+test("formatSpeed", () => {
   store.getState = jest.fn().mockReturnValue({
     session: {
       units: {
         speedBytes: 1000,
         speedUnits: ["kB/s", "MB/s", "GB/s", "TB/s"],
-      }
+      },
     },
-  })
+  });
 
-  expect(formatSpeed(1000)).toBe('1kB/s')
-})
+  expect(formatSpeed(1000)).toBe("1kB/s");
+});
+
+test("formatSpeed format zero", () => {
+  store.getState = jest.fn().mockReturnValue({
+    session: {
+      units: {
+        speedBytes: 1000,
+        speedUnits: ["kB/s", "MB/s", "GB/s", "TB/s"],
+      },
+    },
+  });
+
+  expect(formatSpeed(0, true)).toBe("0kB/s");
+  expect(formatSpeed(0)).toBe(" ");
+});
+
+describe.each`
+  timeStamp     | expected
+  ${0}          | ${" "}
+  ${1610249550} | ${"2021-01-10 11:32:30"}
+  ${1608518614} | ${"2020-12-21 10:43:34"}
+  ${1609340446} | ${"2020-12-30 23:00:46"}
+`("test formatUnixTimeStamp($timeStamp)", ({ timeStamp, expected }) => {
+  test(`returns ${expected}`, () => {
+    expect(formatUnixTimeStamp(timeStamp)).toBe(expected);
+  });
+});
--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,3 +1,5 @@
+import moment from 'moment'
+
 import store from '../store'
 
 export const formatBytes = (number: number, bytes: number, units: string[], digits = 2): string => {
@@ -10,14 +12,27 @@ export const formatBytes = (number: number, bytes: number, units: string[], digi
   return Number(number.toFixed(digits)) + units[unitsIndex]
 }
 
-export const formatSize = (number: number): string => {
+export const formatSize = (number: number, allowZero=false): string => {
   const { session } = store.getState()
   const { sizeBytes, sizeUnits } = session.units
+  if (!allowZero && !number) {
+    return ' '
+  }
   return formatBytes(number, sizeBytes, sizeUnits )
 }
 
-export const formatSpeed = (number: number): string => {
+export const formatSpeed = (number: number, allowZero=false): string => {
   const { session } = store.getState()
   const { speedBytes, speedUnits } = session.units
+  if (!allowZero && !number) {
+    return ' '
+  }
   return formatBytes(number, speedBytes, speedUnits )
+}
+
+export const formatUnixTimeStamp = (timeStamp: number): string => {
+  if (!timeStamp) {
+    return ' '
+  }
+  return moment.unix(timeStamp).format('YYYY-MM-DD HH:mm:ss')
 }
