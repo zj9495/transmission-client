7545066add352e8f854ee74e7425a3fee930d0b8
zj9495 <zj9495@gmail.com>
2021-01-18 13:28:19 +0800

    test(app-start): add unit test

---
+++ src/appStart.test.ts
@@ -0,0 +1,39 @@
+import { LicenseInfo } from "@material-ui/x-grid";
+
+import appStart from "./appStart";
+
+const MOCK_ENV = {
+  REACT_APP_MUIX_LICENSE_KEY: "MOCK_KEY",
+};
+const OLD_ENV = process.env;
+
+describe("test appStart", () => {
+  beforeEach(() => {
+    jest.resetModules(); // most important - it clears the cache
+    process.env = { ...OLD_ENV, ...MOCK_ENV }; // make a copy
+  });
+
+  afterAll(() => {
+    process.env = OLD_ENV; // restore old env
+  });
+  test("should correct initialize", () => {
+    const setupLicenseSpy = jest.spyOn(appStart, "setupLicense");
+
+    appStart.start();
+
+    expect(setupLicenseSpy).toHaveBeenCalled();
+    setupLicenseSpy.mockRestore();
+  });
+
+  test("should correct setup @material-ui/x-grid license key", () => {
+    const setLicenseKeySpy = jest.spyOn(LicenseInfo, "setLicenseKey");
+
+    appStart.setupLicense();
+
+    expect(setLicenseKeySpy).toHaveBeenCalled();
+    expect(setLicenseKeySpy).toHaveBeenCalledWith(
+      MOCK_ENV.REACT_APP_MUIX_LICENSE_KEY
+    );
+    setLicenseKeySpy.mockRestore();
+  });
+});
