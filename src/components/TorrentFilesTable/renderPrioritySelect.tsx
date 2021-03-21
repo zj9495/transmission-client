1b27b91c08a0db310fc75b53a28b84c6545605e1
zj9495 <zj9495@gmail.com>
2021-03-21 22:19:59 +0800

    feat(files): add priority flag

--- src/components/TorrentFilesTable/renderPrioritySelect.tsx
+++ src/components/TorrentFilesTable/renderPrioritySelect.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { FormattedMessage } from "react-intl";
 import { Select, MenuItem } from "@material-ui/core";
 import { GridCellParams } from "@material-ui/x-grid";
-import { FormattedMessage } from "react-intl";
+import FlagIcon from "@material-ui/icons/Flag";
 
 const renderPrioritySelect = (props: GridCellParams) => (
   <Select
@@ -10,12 +11,15 @@ const renderPrioritySelect = (props: GridCellParams) => (
     value={props.value}
   >
     <MenuItem value={1}>
+      <FlagIcon color="secondary" />
       <FormattedMessage id="torrent.attribute.priority.high" />
     </MenuItem>
     <MenuItem value={0}>
+      <FlagIcon color="primary" />
       <FormattedMessage id="torrent.attribute.priority.normal" />
     </MenuItem>
     <MenuItem value={-1}>
+      <FlagIcon color="action" />
       <FormattedMessage id="torrent.attribute.priority.low" />
     </MenuItem>
   </Select>
