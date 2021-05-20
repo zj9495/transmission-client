7bec9b479dc4202fd841191091a65596a78cd00c
zj9495 <zj9495@gmail.com>
2021-05-20 11:17:23 +0800

    test(table): add e2e test for hide the uncommonly used columns: 0412-1108

--- cypress/integration/list.spec.js
+++ cypress/integration/list.spec.js
@@ -3,18 +3,35 @@
 import { removeTestTorrent, addTestTorrent } from "./common"
 import { TEST_TORRENT } from "../fixtures/constants"
 
-context('test torrent list', () => {
+context("test torrent list", () => {
   beforeEach(() => {
     cy.visit(`http://zj9495:zj9495@localhost:8888/transmission/web`)
     cy.verifyConnected()
-    addTestTorrent(false)
   })
 
-  afterEach(() => {
+  it("should format ratio if ratio < 0", () => {
+    addTestTorrent(false)
+
+    cy.contains(TEST_TORRENT.NAME).closest(".MuiDataGrid-row").find("[data-field=uploadRatio]").contains("0")
+
     removeTestTorrent()
   })
 
-  it('should format ratio if ratio < 0', () => {
-    cy.contains(TEST_TORRENT.NAME).closest('.MuiDataGrid-row').find('[data-field=uploadRatio]').contains('0')
+  it("test the default display columns", () => {
+    const COLUMNS = {
+      DISPLAY: ["name", "totalSize", "percentDone", "leftUntilDone", "uploadRatio", "status", "seederCount", "leecherCount", "rateDownload", "rateUpload", "addedDate", "downloadDir"],
+      HIDE: ["completeSize", "uploadedEver", "queuePosition", "trackers", "activityDate", "labels", "doneDate"]
+    }
+
+    cy.viewport(1920, 1080)
+
+    COLUMNS.DISPLAY.forEach(name => {
+      // should be display
+      cy.get(`[data-field=${name}]`).should("be.exist")
+    })
+    COLUMNS.HIDE.forEach(name => {
+      // should be display
+      cy.get(`[data-field=${name}]`).should("not.exist")
+    })
   })
 })
