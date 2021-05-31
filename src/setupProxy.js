8b707a97a831bc49b472a3053665de4e5c015c89
zj9495 <zj9495@gmail.com>
2021-05-31 14:04:24 +0800

    ci(e2e): add proxy deplay

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,21 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
+const proxyDelay = function (req, res, next) {
+  const isLocalhost = /localhost|127.0.0.1/.test(
+    process.env.REACT_APP_TRANSMISSION_RPC
+  );
+  if (process.env.CI && isLocalhost) {
+    // Delay request by 0.1 seconds
+    setTimeout(next, 100);
+  } else {
+    next();
+  }
+};
 module.exports = function (app) {
   app.use(
     "/transmission/rpc",
+    proxyDelay,
     createProxyMiddleware({
       target: process.env.REACT_APP_TRANSMISSION_RPC,
       changeOrigin: true,
