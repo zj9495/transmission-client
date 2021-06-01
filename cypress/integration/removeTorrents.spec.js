6e9eb96741a3001a47fac79b547ef2e0c36f0e78
zj9495 <zj9495@gmail.com>
2021-06-01 15:28:29 +0800

    test(table): add e2e test for clear the selection

--- cypress/integration/removeTorrents.spec.js
+++ cypress/integration/removeTorrents.spec.js
@@ -21,4 +21,20 @@ context('app', () => {
     cy.contains('Successfully removed!')
     cy.contains(TEST_TORRENT.NAME).should('not.exist')
   })
+
+  it('should clear the selection after by multiple delete', () => {
+    addTestTorrent()
+    
+    cy.contains(TEST_TORRENT.NAME).closest('.MuiDataGrid-row').find('input[type=checkbox]').check()
+    cy.get('.MuiDataGrid-columnsContainer input[type=checkbox]').should('be.checked')
+
+    cy.getByTestId('delete-btn').click()
+    cy.contains('Remove confirm')
+    cy.get('[data-testid=delete-local-data] [type=checkbox]').check()
+    cy.contains('OK').click()
+    cy.contains('Successfully removed!')
+
+    cy.get('.MuiDataGrid-columnsContainer input[type=checkbox]').should('not.checked')
+    cy.getByTestId('delete-btn').should('be.disabled')
+  })
 })
