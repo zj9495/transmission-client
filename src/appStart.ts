64596f57208ef9717a00cec5dbded1148bfd3293
zj9495 <zj9495@gmail.com>
2021-08-20 16:51:55 +0800

    refactor(redux): remove the use of localStorage

--- src/appStart.ts
+++ src/appStart.ts
@@ -2,8 +2,8 @@
 import { LicenseInfo } from "@material-ui/x-grid";
 
 import store from "src/store";
-import { setLocale, toggleTheme } from "src/store/actions/rpc";
-import { DEFAULT_LANGUAGE, STORAGE_KEYS } from "src/constants";
+import { setLocale } from "src/store/actions/rpc";
+import { DEFAULT_LANGUAGE } from "src/constants";
 import { formatLocale } from "src/utils/formatter";
 import qs from "query-string";
 
@@ -13,13 +13,11 @@ class AppStart {
   }
   setupLocale() {
     const localeFromQs = qs.parse(window.location.search).locale as string;
-    const localeFromStorage = window.localStorage[STORAGE_KEYS.LOCALE];
     const localeFromStore = store.getState().rpc.locale;
     const localeFromNavigator = formatLocale(navigator.language, false);
     const locale =
       formatLocale(localeFromQs, false) ||
       localeFromStore ||
-      localeFromStorage ||
       localeFromNavigator ||
       DEFAULT_LANGUAGE.code;
     setLocale(locale)(store.dispatch);
@@ -36,16 +34,9 @@ class AppStart {
       );
     }
   }
-  setupTheme() {
-    const themeFromStorage = window.localStorage[STORAGE_KEYS.THEME];
-    if (themeFromStorage) {
-      toggleTheme(themeFromStorage)(store.dispatch, store.getState);
-    }
-  }
   start() {
     this.setupLicense();
     this.setupLocale();
-    this.setupTheme();
   }
 }
 
